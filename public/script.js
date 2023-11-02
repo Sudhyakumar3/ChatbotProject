f5dae68fbaf589bf00fefe3c7fb148e36ff02a76
Subhasini Udhyakumar <sudhyakumar3@gatech.edu>
2023-11-02 01:33:44 -0400

    Added title, updated typing indicator, and more

--- public/script.js
+++ public/script.js
@@ -1,21 +1,34 @@
 // JavaScript code
+
+// Get references to HTML elements
 const chat = document.getElementById('chat');
 const userMessageInput = document.getElementById('user-message');
 const sendButton = document.getElementById('send');
 const typingIndicator = document.getElementById('typing-indicator');
 
+// Hide the typing indicator initially
 typingIndicator.style.display = 'none';
 
-appendMessage('Gordon Ramsey','Hey, Mate! Enter an assignment to get started!', 'chatgpt-message'); 
+// Initialize the conversation with a greeting message
+appendMessage('Gordon Ramsey', 'Hey, mate! What questions do you have for me?', 'chatgpt-message');
 
+// Add a submit event listener to the chat form
 document.getElementById('chatForm').addEventListener('submit', async function (event) {
     event.preventDefault();
+
+    // Get the user's input message
     const userInput = userMessageInput.value;
 
-        // Show the typing indicator when the user sends a message
-        typingIndicator.style.display = 'inline-block';
+    // Clear the user's input field
+    userMessageInput.value = '';
 
+    // Display the user's message in the chat
+    appendMessage('You', userInput, 'user-message');
 
+    // Show the typing indicator when the user sends a message
+    typingIndicator.style.display = 'inline-block';
+
+    // Send the user's message to the server for a response
     const response = await fetch('/api/chat', {
         method: 'POST',
         headers: {
@@ -24,20 +37,20 @@ document.getElementById('chatForm').addEventListener('submit', async function (e
         body: JSON.stringify({ userInput })
     });
 
+    // Get the response from the server
     const data = await response.json();
+    
     // Hide the typing indicator when the response is received
     typingIndicator.style.display = 'none';
 
-    appendMessage('You', userInput, 'user-message');
+    // Display the chatbot's response in the chat
     appendMessage('Gordon Ramsey', data.message, 'chatgpt-message');
-    userMessageInput.value = '';
 });
 
+// Function to append a message to the chat container
 function appendMessage(sender, message, messageClass) {
     const messageDiv = document.createElement('div');
     messageDiv.classList.add('message', messageClass);
     messageDiv.innerHTML = `<strong>${sender}:</strong> ${message}`;
     chat.appendChild(messageDiv);
 }
-
-
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-//use npm to install express and openai packages
-
+// Import required packages using npm
 const express = require('express');
 const app = express();
 const OpenAI = require('openai');
-const openai = new OpenAI({ apiKey:'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'}); //replace 'API Key' with your API key
 
+// Initialize OpenAI with your API key
+const openai = new OpenAI({ apiKey: 'API Key'}); // Replace 'API Key' with your API key
+
+// Configure Express middleware
 app.use(express.json());
 app.use(express.static('public'));
 
+// Handle POST requests to the '/api/chat' endpoint
 app.post('/api/chat', async (req, res) => {
     const userInput = req.body.userInput;
+    
+    // Create a chat conversation with OpenAI
     const response = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [
@@ -18,10 +23,14 @@ app.post('/api/chat', async (req, res) => {
         ],
     });
 
+    // Send the chatbot's response as a JSON object
     res.json({ message: response.choices[0].message.content });
 });
 
+// Define the port for the server to listen on
 const PORT = 3000;
+
+// Start the Express server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
